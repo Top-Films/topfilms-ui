87aa22dcb9bd0d94bbcf02d12a9babaabbab60c4
Max Morhardt <70174942+maxmorhardt@users.noreply.github.com>
2024-01-16 17:54:18 -0500

    comments for redeploy

--- src/util/Environment.ts
+++ src/util/Environment.ts
@@ -1,16 +1,19 @@
 export class Environment {
+	// Get auth api url depending on the env
 	static getAuthAPIUrl(): string {
 		return import.meta.env.PROD 
 			? 'https://auth.topfilms.co'
 			: 'http://localhost:3001';
 	}
 
+	// Get frontend url depending on the env
 	static getFrontendUrl(): string {
 		return import.meta.env.PROD 
 			? 'https://topfilms.co'
 			: 'http://localhost:3000';
 	}
 
+	// Get primary api depending on the env
 	static getAPIUrl(): string {
 		return import.meta.env.PROD 
 			? 'https://api.topfilms.co'
